5b2e3b14671f7a844ec77b6854957c4a7611a01e
Damian Baćkowski <damianbackowski@gmail.com>
2012-01-27 18:59:05 +0100

    Migrated project to RoR 3.2.1, fixed bugs, center pagination links.

--- Gemfile
+++ Gemfile
@@ -1,21 +1,21 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.3'
+gem 'rails', '3.2.1'
 
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'mysql2'
 gem 'therubyracer'
-gem 'will_paginate', '~> 3.0.2'
+gem 'will_paginate'
 gem 'foreigner'
 gem 'RedCloth'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails',   '~> 3.1.4'
-  gem 'coffee-rails', '~> 3.1.1'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
 
---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,36 @@
+require 'will_paginate/view_helpers/action_view'
+
+# config/initializers/will_paginate.rb
+# 
+# This extension code was written by Isaac Bowen, originally found
+# at http://isaacbowen.com/blog/using-will_paginate-action_view-and-bootstrap/
+
+module WillPaginate
+  module ActionView
+    def will_paginate(collection = nil, options = {})
+      options[:renderer] ||= BootstrapLinkRenderer
+      options[:class] = 'pagination center'
+      super.try :html_safe
+    end
+
+    class BootstrapLinkRenderer < LinkRenderer
+      protected
+      
+      def html_container(html)
+        tag :div, tag(:ul, html), container_attributes
+      end
+
+      def page_number(page)
+        tag :li, link(page, page, :rel => rel_value(page)), :class => ('active' if page == current_page)
+      end
+
+      def gap
+        tag :li, link(super, '#'), :class => 'disabled'
+      end
+
+      def previous_or_next_page(page, text, classname)
+        tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+      end
+    end
+  end
+end
